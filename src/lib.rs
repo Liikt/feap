6188650c5ef62db0d852f74e0d60c17577c941a5
Liikt <jb@futureplay.de>
2022-08-18 18:19:40 +0200

    works? + clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,110 +1,79 @@
-use std::rc::Rc;
-use std::cell::RefCell;
+use std::{ptr, cmp::Ordering};
 
 const MAX_DEGREE: usize = 0x20;
 
+type Link<T> = *mut Node<T>;
+
+macro_rules! node {
+    ($node:expr) => {
+        unsafe { Box::from_raw($node) }
+    };
+}
+
 #[derive(Debug, Clone)]
-struct InnerNode<T> {
-    parent:   Option<Node<T>>,
-    children: Vec<Node<T>>,
+struct Node<T> {
+    parent:   Option<Link<T>>,
+    children: Vec<Link<T>>,
     degree:   u8,
+    marked:   bool,
 
     pub val: T
 }
 
-impl<T: PartialEq> PartialEq for InnerNode<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.val == other.val
-    }
-}
-
-impl<T: Eq> Eq for InnerNode<T> {}
-
-impl<T: PartialOrd> PartialOrd for InnerNode<T> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.val.partial_cmp(&other.val)
-    }
-}
-
-impl<T: Ord> Ord for InnerNode<T> {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.val.cmp(&other.val)
-    }
-
-    fn min(self, other: Self) -> Self
-        where
-            Self: Sized, {
-        if self.val <= other.val {
-            self
-        } else {
-            other
-        }
-    }
-}
-
-impl<T: PartialEq + Eq + PartialOrd + Ord> InnerNode<T> {
+impl<T: PartialEq + Eq + PartialOrd + Ord> Node<T> {
     fn new(val: T) -> Self {
         Self {
             parent:   None,
             children: Vec::new(),
             degree:   0,
+            marked:   false,
             val
         }
     }
 }
 
-type Node<T> = Rc<RefCell<InnerNode<T>>>; 
-
+#[derive(Debug)]
 pub struct FibHeap<T> {
-    min: Option<Node<T>>,
-    head_list: Vec<Node<T>>
+    min: Link<T>,
+    head_list: Vec<Link<T>>
+}
+
+impl<T: PartialEq + Eq + PartialOrd + Ord + Clone> Default for FibHeap<T> {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl<T: PartialEq + Eq + PartialOrd + Ord + Clone> FibHeap<T> {
     pub fn new() -> Self {
         Self {
-            min: None,
+            min: ptr::null_mut(),
             head_list: Vec::new()
         }
     }
 
-    pub fn get_min(&self) -> Option<T> {
-        if let Some(min) = &self.min {
-            return Some(RefCell::borrow(min).val.clone());
-        }
-        None
+    pub fn get_min(&self) -> Option<&T> {
+        unsafe {self.min.as_ref()}.map(|m| &m.val)
     }
 
     pub fn insert(&mut self, val: T) {
-        let new = Rc::new(RefCell::new(InnerNode::new(val)));
+        let new = Box::into_raw(Box::new(Node::new(val)));
         self.insert_node(new);
     }
 
-    fn insert_node(&mut self, new: Node<T>) {
-        self.min.as_ref().map_or(Some(new.clone()), |min| {
-            if RefCell::borrow(&new).val < RefCell::borrow(min).val {
-                return Some(new.clone());
-            }
-            Some(min.clone())
-        });
+    fn insert_node(&mut self, new: Link<T>) {
+        if self.min.is_null() || node!(new).val < node!(self.min).val {
+            self.min = new;
+        }
+
         self.head_list.push(new);
     }
 
-    pub fn extract_min(&mut self) -> Option<T> {
-        if self.min.is_none() {
-            return None;
-        }
-
-        // Remove all children from min
-        for c in &self.min.as_ref().unwrap().borrow().children {
-            self.head_list.push(c.clone());
-            drop(c);
-        }
-
+    fn cleanup(&mut self) {
         // Merge trees
-        let mut root_list: Vec<Option<Node<T>>> = vec![None; MAX_DEGREE];
-        for c in &self.head_list {
-            let mut tmp = insert_root_list(c.clone(), &mut root_list);
+        let mut root_list: Vec<Option<Link<T>>> = vec![None; MAX_DEGREE];
+        for &c in &self.head_list {
+            let mut tmp = insert_root_list(c, &mut root_list);
             while tmp.is_some() {
                 tmp = insert_root_list(tmp.unwrap(), &mut root_list);
             }
@@ -114,61 +83,97 @@ impl<T: PartialEq + Eq + PartialOrd + Ord + Clone> FibHeap<T> {
         self.head_list.clear();
         for c in &root_list {
             if c.is_some() {
-                self.insert_node(c.as_ref().unwrap().clone());
+                self.insert_node(c.unwrap());
             }
         }
-
-        Some(self.min.as_ref().unwrap().borrow().val.clone())
     }
 
-    fn find_elem(&self, cur_node: Node<T>, val: T) -> Option<Node<T>> {
-        for c in &RefCell::borrow(&cur_node).children {
-            let child_val = RefCell::borrow(&c).val.clone();
-            if child_val == val { return Some(c.clone()) }
-            else if child_val < val { return self.find_elem(c.clone(), val); }
+    pub fn extract_min(&mut self) -> Option<T> {
+        if self.min.is_null() {
+            return None;
+        }
+
+        // Remove all children from min
+        for c in node!(self.min).children {
+            self.head_list.push(c);
+            node!(c).parent = None;
+        }
+
+        if self.head_list.len() > 8 {
+            self.cleanup();
+        }
+
+        Some(node!(self.min).val)
+    }
+
+    fn find_elem(&self, cur_node: Link<T>, val: &T) -> Option<Link<T>> {
+        for &c in &node!(cur_node).children {
+            match node!(c).val.cmp(val) {
+                Ordering::Equal => return Some(c),
+                Ordering::Less => return self.find_elem(c, val),
+                _ => {}
+            }
         }
         None
     }
 
+    fn cut_out(&mut self, node: Link<T>) {
+        node!(node).marked = false;
+        if let Some(parent) = node!(node).parent {
+            self.insert_node(node);
+            let idx = node!(parent).children.iter()
+                .position(|&v| node!(v).val == node!(node).val)
+                .unwrap();
+            node!(parent).children.remove(idx);
+            if !node!(parent).marked {
+                node!(parent).marked = true;
+            } else {
+                self.cut_out(parent);
+            }
+        }
+    }
+
+
     pub fn decrease_key(&mut self, old_val: T, new_val: T) {
         let mut cur_node = None;
-        for t in &self.head_list {
-            cur_node = self.find_elem(t.clone(), old_val.clone());
+        for &t in &self.head_list {
+            cur_node = self.find_elem(t, &old_val);
             if cur_node.is_some() { break; }
         }
-        let cur_node = cur_node.as_ref().unwrap();
-        RefCell::borrow_mut(&cur_node).val = new_val.clone();
-        let parent = RefCell::borrow(&cur_node).parent.clone();
-        if parent.is_some() && RefCell::borrow(&parent.as_ref().unwrap()).val >= new_val {
-            let parent = parent.as_ref().unwrap();
-            
+
+        if let Some(cur_node) = cur_node {
+            let parent = node!(cur_node).parent;
+            if parent.is_some() && node!(parent.unwrap()).val >= new_val {
+                self.cut_out(cur_node);
+            }
         }
+
     } 
 }
 
-fn insert_root_list<T>(link: Node<T>, root_list: &mut Vec<Option<Node<T>>>) -> Option<Node<T>> 
+fn insert_root_list<T>(link: Link<T>, root_list: &mut [Option<Link<T>>]) -> Option<Link<T>> 
     where
         T: PartialEq + Eq + PartialOrd + Ord + Clone {
-    let cur_spot = RefCell::borrow(&link).degree as usize;
+    let cur_spot = node!(link).degree as usize;
     if root_list[cur_spot].is_none() {
-        root_list[cur_spot] = Some(link.clone());
-        return None;
+        root_list[cur_spot] = Some(link);
+        None
     } else {
-        let v1 = RefCell::borrow(&link).val.clone();
+        let v1 = node!(link).val;
         let v2 = 
-            RefCell::borrow(&root_list[cur_spot].as_ref().unwrap())
+            node!(root_list[cur_spot].unwrap())
             .val
             .clone();
         let (min, max) = if v1 < v2 { 
-            (link.clone(), root_list[cur_spot].as_ref().unwrap().clone())
+            (link, root_list[cur_spot].unwrap())
         } else { 
-            (root_list[cur_spot].as_ref().unwrap().clone(), link.clone())
+            (root_list[cur_spot].unwrap(), link)
         };
 
-        RefCell::borrow_mut(&min).children.push(max);
-        RefCell::borrow_mut(&min).degree += 1;
+        node!(min).children.push(max);
+        node!(min).degree += 1;
         root_list[cur_spot] = None;
-        return Some(min.clone());
+        Some(min)
     }
 }
 
